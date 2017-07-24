82f32a4686a7d9917559c3b5383bc1119feb6094
Richard Cosgrove <richardgcosgrove@gmail.com>
2017-07-23 20:47:21 -0400

    working reducers

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => {
+      return post.code === postId;
+    });
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-      single photo
+        <Photo i={i} post={post} key={i} {...this.props} />
+        <Comments postComments={postComments} {...this.props}></Comments>
       </div>
     );
   }
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,6 +3,16 @@
 // 2. copy of current state
 
 export default function posts(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ];
+
+    default:
+      return state;
+  }
 }
